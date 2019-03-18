bcd70800fa99240a9a0727298aaed51ad724d3d2
Felipe Machado <felipe.machado.ads@gmail.com>
2019-03-18 17:55:44 -0300

    Refatorando os modais em apenas um

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ class Despesa {
         this.valor = valor
     }
 
-     validarDados() {
-        for(let i  in this){
-           if(this[i]== undefined ||this[i]==''|| this[i] == null){
-               return false;
-           }
+    validarDados() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false;
+            }
         }
         return true;
     }
@@ -21,19 +21,19 @@ class Despesa {
 
 class Bd {
     constructor() {
-            let id = localStorage.getItem('id')
-            if(id === null) {
-                    localStorage.setItem('id', 0)
-            }
+        let id = localStorage.getItem('id')
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
     }
     getProximoId() {
-            let proximoId = localStorage.getItem('id') // null
-            return parseInt(proximoId) + 1
+        let proximoId = localStorage.getItem('id') // null
+        return parseInt(proximoId) + 1
     }
     gravar(d) {
-            let id = this.getProximoId()
-            localStorage.setItem(id, JSON.stringify(d))
-            localStorage.setItem('id', id)
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
     }
 }
 let bd = new Bd()
@@ -56,13 +56,24 @@ function cadastrarDespesas() {
         valor.value
 
     )
-    if(despesa.validarDados()){
+    if (despesa.validarDados()) {
         bd.gravar(despesa)
-      
-        $('#sucessoGravacao').modal('show')
-    }else{
-        $('#erroGravacao').modal('show')
+
+        document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+       document.getElementById('modal_btn').innerHTML = 'voltar' 
+       document.getElementById('modal_btn').className = 'btn btn-success'    
+
+        $('#modalRegistraDespesa').modal('show')
+    } else {
+        document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão dos registros'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro despesa não registrada'
+        document.getElementById('modal_btn').innerHTML = 'voltar e corrigir' 
+       document.getElementById('modal_btn').className = 'btn btn-danger'    
+        $('#modalRegistraDespesa').modal('show')
     }
-    
+
 }
 
