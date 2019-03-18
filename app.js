dacbb0fb0b0c4bb87d5dedaa15f0bc32db754859
Felipe Machado <felipe.machado.ads@gmail.com>
2019-03-18 15:08:52 -0300

    Validando campos do formulario com modal

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+     validarDados() {
+        for(let i  in this){
+           if(this[i]== undefined ||this[i]==''|| this[i] == null){
+               return false;
+           }
+        }
+        return true;
+    }
 }
 
 
@@ -47,6 +56,13 @@ function cadastrarDespesas() {
         valor.value
 
     )
-    bd.gravar(despesa)
+    if(despesa.validarDados()){
+        bd.gravar(despesa)
+      
+        $('#sucessoGravacao').modal('show')
+    }else{
+        $('#erroGravacao').modal('show')
+    }
+    
 }
 
